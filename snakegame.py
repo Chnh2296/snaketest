d4d702b8ea45ce19655fe97c543e49216c9c0d7f
Chnh2296 <Chinh.cena2009@gmail.com>
2025-04-05 11:36:02 +0700

    Update snakegame.py

--- snakegame.py
+++ snakegame.py
@@ -3,6 +3,14 @@ import pygame as pg
 from random import randrange
 
 WINDOW = 1000
+TITLE_SIZE = 50
+RANGE = (TITLE_SIZE // 2, WINDOW - TITLE_SIZE // 2, TITLE_SIZE)
+get_random_position = lambda: [randrange(*RANGE), randrange(*RANGE)]
+snake = pg.rect.Rect([0, 0, TITLE_SIZE - 2, TITLE_SIZE - 2])
+snake.center = get_random_position()
+length = 1
+segments = [snake.copy()]
+
 screen = pg.display.set_mode([WINDOW] * 2)
 clock = pg.time.Clock()
 
@@ -11,5 +19,6 @@ while True:
     if event.type == pg.QUIT:
       exit()
   screen.fill('black')
+  [pg.draw.rect(screen, 'green', segment in segments]
   pg.display.flip()
   clock.tick(60)
