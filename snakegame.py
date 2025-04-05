d10aadc092e96054d7f6b3fa2502cf993c76f7b0
Chnh2296 <Chinh.cena2009@gmail.com>
2025-04-05 12:46:29 +0700

    Update snakegame.py

--- snakegame.py
+++ snakegame.py
@@ -10,7 +10,7 @@ snake = pg.rect.Rect([0, 0, TITLE_SIZE - 2, TITLE_SIZE - 2])
 snake.center = get_random_position()
 length = 1
 segments = [snake.copy()]
-
+snake_dir = (0, 0)
 screen = pg.display.set_mode([WINDOW] * 2)
 clock = pg.time.Clock()
 
@@ -18,7 +18,22 @@ while True:
   for event in pg.event.get():
     if event.type == pg.QUIT:
       exit()
+    if event.type == PG.KEYDOWN:
+      if event.key == pg.K_w:
+        snake_dir = (0, -TITLE_SIZE)
+      if event.key == pg.K_s:
+        snake_dir = (0, TITLE_SIZE)
+      if event. key == pg.K_a:
+        snake_dir = (-TITLE_SIZE, 0)
+      if event.key == pg.K_d:
+        snake_dir = (TITLE_SIZE, 0)
+        
   screen.fill('black')
+  #vẽ rắn
   [pg.draw.rect(screen, 'green', segment) for segment in segments]
+  #di chuyển
+  snake.move_ip(snake_dir)
+  segments.append(snake.copy())
+  segments = segments[-length:]
   pg.display.flip()
   clock.tick(60)
