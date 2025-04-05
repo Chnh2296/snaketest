95f6f101092268b30a9785c21797fba0eba1ae91
Chnh2296 <Chinh.cena2009@gmail.com>
2025-04-05 12:49:53 +0700

    Update snakegame.py

--- snakegame.py
+++ snakegame.py
@@ -18,7 +18,7 @@ while True:
   for event in pg.event.get():
     if event.type == pg.QUIT:
       exit()
-    if event.type == PG.KEYDOWN:
+    if event.type == pg.KEYDOWN:
       if event.key == pg.K_w:
         snake_dir = (0, -TITLE_SIZE)
       if event.key == pg.K_s:
